18bdb985e44d39642eef5d8d4410ab60913798bf
ssup_sam <sarl.2001@hotmail.com>
2024-11-02 18:53:39 -0300

    wip: creates main files

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import './globals.css';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main>text</main>
+
+      <Footer />
+    </>
+  );
+}
